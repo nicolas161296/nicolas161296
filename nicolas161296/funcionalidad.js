a6cc109c38b18e751b45f816a8f739c27d8dc7c2
nicolas161296 <127266216+nicolas161296@users.noreply.github.com>
2023-03-30 18:00:37 -0300

    cv-interactivo

---
+++ nicolas161296/funcionalidad.js
@@ -0,0 +1,19 @@
+
+document.getElementById('perfil').onclick = function () {
+    document.getElementById('texto').style.display='inline'
+}
+document.getElementById('educacion').onclick = function () {
+    document.getElementById('texto1').style.display='inline'
+}
+document.getElementById('cursos').onclick = function () {
+    document.getElementById('texto2').style.display='inline'
+}
+document.getElementById('exp').onclick = function () {
+    document.getElementById('texto3').style.display='inline'
+}
+document.getElementById('habilidades').onclick = function () {
+    document.getElementById('texto4').style.display='inline'
+}
+document.getElementById('aspectos').onclick = function () {
+    document.getElementById('texto5').style.display='inline'
+}
